1b1596fe8ad64f3196e2bc90838bd5926ee22727
Wishcle <cpmqaq@gmail.com>
2024-12-08 13:50:48 -0500

    day3: part2: solution

--- day-03-mull-it-over/main.py
+++ day-03-mull-it-over/main.py
@@ -15,6 +15,25 @@ def process_mul(statement: str) -> int:
     return a * b
 
 
+def process_instructions(instructions: List[str]) -> int:
+    result = 0
+    muls_enabled = True
+
+    for instruction in instructions:
+        match instruction:
+            case "do()":
+                muls_enabled = True
+            case "don't()":
+                muls_enabled = False
+                pass
+            case _:
+                assert "mul" in instruction, f"{instruction=}"
+                if muls_enabled:
+                    result += process_mul(instruction)
+
+    return result
+
+
 def part1() -> int:
     corrupt_data = ''.join(get_input_lines())
     muls = re.findall(r"mul\(\d{1,3},\d{1,3}\)", corrupt_data)
@@ -22,8 +41,24 @@ def part1() -> int:
     return result
 
 
+def part2() -> int:
+    corrupt_data = ''.join(get_input_lines())
+
+    pattern = (
+        r"(mul\(\d{1,3},\d{1,3}\))"
+        r"|(do\(\))"
+        r"|(don't\(\))"
+    )
+
+    instruction_tuples = re.findall(pattern, corrupt_data)
+    instructions = [''.join(t) for t in instruction_tuples]
+    result = process_instructions(instructions)
+    return result
+
+
 def main():
     print(f"PART 1 SOLUTION: {part1()}")
+    print(f"PART 2 SOLUTION: {part2()}")
 
 
 if __name__ == "__main__":
