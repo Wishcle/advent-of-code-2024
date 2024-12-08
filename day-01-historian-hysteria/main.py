5349923059a0bdc536bde2d0f971df70a74b6230
Wishcle <cpmqaq@gmail.com>
2024-12-08 11:28:51 -0500

    day1: part2: solution

--- day-01-historian-hysteria/main.py
+++ day-01-historian-hysteria/main.py
@@ -1,4 +1,5 @@
 
+from collections import Counter
 from typing import List
 
 
@@ -7,7 +8,7 @@ def read_input() -> List[str]:
         return f.readlines()
 
 
-def main():
+def part1() -> int:
     pair_strings = read_input()
 
     pairs = [ps.split() for ps in pair_strings]
@@ -18,7 +19,30 @@ def main():
 
     distances = [abs(e1 - e2) for e1, e2 in zip(list1, list2)]
     solution = sum(distances)
-    print(f"SOLUTION: {solution}")
+    return solution
+
+
+def part2() -> int:
+    pair_strings = read_input()
+
+    pairs = [ps.split() for ps in pair_strings]
+    list1 = [int(p[0]) for p in pairs]
+    list2 = [int(p[1]) for p in pairs]
+
+    list1_counts = Counter(list1)
+    list2_counts = Counter(list2)
+
+    total_similarity_score = 0
+    for k, v in list1_counts.items():
+        score = k * v * (list2_counts.get(k, 0))
+        total_similarity_score += score
+
+    return total_similarity_score
+
+
+def main():
+    print(f"PART 1 SOLUTION: {part1()}")
+    print(f"PART 2 SOLUTION: {part2()}")
 
 
 if __name__ == "__main__":
