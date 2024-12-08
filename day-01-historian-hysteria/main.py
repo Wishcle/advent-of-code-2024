1e14ae381b1527936b9f3a3ffa7ba220eeeaf8fd
Wishcle <cpmqaq@gmail.com>
2024-12-08 11:36:02 -0500

    day1: refactor

--- day-01-historian-hysteria/main.py
+++ day-01-historian-hysteria/main.py
@@ -1,34 +1,30 @@
 
 from collections import Counter
-from typing import List
+from typing import List, Tuple
 
 
-def read_input() -> List[str]:
+def get_input_lines() -> List[str]:
     with open("input.txt", "r") as f:
         return f.readlines()
 
 
-def part1() -> int:
-    pair_strings = read_input()
-
+def get_input_lists() -> Tuple[List[str], List[str]]:
+    pair_strings = get_input_lines()
     pairs = [ps.split() for ps in pair_strings]
     list1 = [int(p[0]) for p in pairs]
     list2 = [int(p[1]) for p in pairs]
-    list1.sort()
-    list2.sort()
+    return list1, list2
 
+
+def part1() -> int:
+    list1, list2 = get_input_lists()
     distances = [abs(e1 - e2) for e1, e2 in zip(list1, list2)]
     solution = sum(distances)
     return solution
 
 
 def part2() -> int:
-    pair_strings = read_input()
-
-    pairs = [ps.split() for ps in pair_strings]
-    list1 = [int(p[0]) for p in pairs]
-    list2 = [int(p[1]) for p in pairs]
-
+    list1, list2 = get_input_lists()
     list1_counts = Counter(list1)
     list2_counts = Counter(list2)
 
