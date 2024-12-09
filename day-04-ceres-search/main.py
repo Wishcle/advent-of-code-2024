8f9ef50f902ad05103fb4c66fc2b392f1d780acb
Wishcle <cpmqaq@gmail.com>
2024-12-09 11:58:33 -0500

    day4: part2: solution

--- day-04-ceres-search/main.py
+++ day-04-ceres-search/main.py
@@ -43,6 +43,25 @@ def count_xmas(grid: List[str], x: int, y: int) -> int:
     return count
 
 
+def count_x_mas(grid: List[str], x: int, y: int) -> int:
+    """Return the number of X-MASs found starting at (x, y) in the grid (0 or 1)."""
+    if not check_letter(grid, "A", x, y):
+        return 0
+
+    mas_ne_sw = (
+        (check_letter(grid, "M", x - 1, y - 1) and check_letter(grid, "S", x + 1, y + 1)) or
+        (check_letter(grid, "S", x - 1, y - 1) and check_letter(grid, "M", x + 1, y + 1)))
+
+    mas_nw_se = (
+        (check_letter(grid, "M", x + 1, y - 1) and check_letter(grid, "S", x - 1, y + 1)) or
+        (check_letter(grid, "S", x + 1, y - 1) and check_letter(grid, "M", x - 1, y + 1)))
+
+    if mas_ne_sw and mas_nw_se:
+        return 1
+
+    return 0
+
+
 def part1() -> int:
     grid = get_input_lines()
     occurrences = 0
@@ -54,8 +73,18 @@ def part1() -> int:
     return occurrences
 
 
+def part2() -> int:
+    grid = get_input_lines()
+    occurrences = 0
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            occurrences += count_x_mas(grid, x, y)
+    return occurrences
+
+
 def main():
     print(f"PART 1 SOLUTION: {part1()}")
+    print(f"PART 2 SOLUTION: {part2()}")
 
 
 if __name__ == "__main__":
