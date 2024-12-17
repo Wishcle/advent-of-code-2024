602a848f411ce5d6a91fde3ff8a4f316c5d37c52
Wishcle <cpmqaq@gmail.com>
2024-12-17 15:43:14 -0500

    day6: part2: solution

--- day-06-guard-gallivant/main.py
+++ day-06-guard-gallivant/main.py
@@ -1,12 +1,14 @@
+from tqdm import tqdm
 from typing import List, Set, Tuple
 
 Position = Tuple[int, int]
+Direction = Tuple[int, int]
 
 
 class Guard:
     position: Position
-    direction_index: int = 0
-    directions: List[Tuple[int, int]] = [
+    direction_index: int
+    directions: List[Direction] = [
         (0, -1),
         (1, 0),
         (0, 1),
@@ -14,26 +16,44 @@ class Guard:
     ]
 
     distinct_positions: Set[Position] = None
+    distinct_positions_with_direction: Set[Tuple[Position, Direction]] = None
+    original_position: Position
 
     def __init__(self, pos: Position):
-        self.position = pos
-        self.distinct_positions = {pos}
+        self.original_position = pos
+        self.reset()
 
     def next_pos(self) -> Position:
         x, y = self.position
-        dx, dy = self.directions[self.direction_index]
+        dx, dy = self.get_direction()
         return x + dx, y + dy
 
+    def get_direction(self) -> Direction:
+        return self.directions[self.direction_index]
+
     def move(self) -> None:
+        self.distinct_positions_with_direction.add((self.position, self.get_direction()))
+
         next_pos = self.next_pos()
         self.position = next_pos
         self.distinct_positions.add(next_pos)
 
     def rotate(self) -> None:
+        self.distinct_positions_with_direction.add((self.position, self.get_direction()))
         self.direction_index = (self.direction_index + 1) % len(self.directions)
 
     def get_distinct_positions(self) -> int:
         return len(self.distinct_positions)
+
+    def has_been_here_before(self) -> bool:
+        return (self.position, self.get_direction()) in self.distinct_positions_with_direction
+
+    def reset(self) -> None:
+        pos = self.original_position
+        self.position = pos
+        self.direction_index = 0
+        self.distinct_positions = {pos}
+        self.distinct_positions_with_direction = set()
 
 
 class Map:
@@ -69,11 +89,11 @@ class Map:
         self.guard = guard
         assert guard is not None
 
-    def simulate_guard(self, obstacles_to_add: List[Position] = None) -> int:
-        """Return distinct positions of the guard. If loop, return -1."""
-        assert obstacles_to_add is None, "unimplemented"
-        obstacles_to_add = obstacles_to_add or []
-        obstacles = list(self.obstacles) + obstacles_to_add
+    def simulate_guard(self, obstacle_to_add: List[Position] = None) -> int:
+        """Return distinct positions of the guard. If loop, return 0."""
+        self.guard.reset()
+        obstacle_to_add = obstacle_to_add or []
+        obstacles = list(self.obstacles) + obstacle_to_add
 
         # if next pos is off the map, we are done. return distinct positions
         # if next pos is an obstacle, rotate guard
@@ -88,8 +108,32 @@ class Map:
                     return self.guard.get_distinct_positions()
                 case (x, y) if (x, y) in obstacles:
                     self.guard.rotate()
+                    if self.guard.has_been_here_before():
+                        return -1
                 case _:
                     self.guard.move()
+                    if self.guard.has_been_here_before():
+                        return -1
+
+    def simulate_all_obstacle_placements(self) -> int:
+        """Return the number of obstacle placements that create loops."""
+        loops = 0
+
+        # First, get all unique locations of the guard. Any single new obstacle
+        # that would create a loop would have to be reachable by the guard.
+        self.simulate_guard()
+        potential_loop_obstacles = set(self.guard.distinct_positions)
+
+        with tqdm(total=(self.h * self.w)) as progress:
+            for y in range(self.h):
+                for x in range(self.w):
+                    progress.update()
+                    if (x, y) not in potential_loop_obstacles:
+                        continue
+                    if self.simulate_guard([(x, y)]) == -1:
+                        loops += 1
+
+        return loops
 
 
 def get_input_lines() -> List[str]:
@@ -103,8 +147,15 @@ def part1() -> int:
     return map.simulate_guard()
 
 
+def part2() -> int:
+    text = get_input_lines()
+    map = Map(text)
+    return map.simulate_all_obstacle_placements()
+
+
 def main():
     print(f"PART 1 SOLUTION: {part1()}")
+    print(f"PART 2 SOLUTION: {part2()}")
 
 
 if __name__ == "__main__":
